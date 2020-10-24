e263460a484a61648c9565a10356d4f25e703906
Saeed Rajabalizade <saeedalize@gmail.com>
2020-10-25 01:21:12 +0330

    init commit. make ex02 project

---
+++ D0/ex02/ex02/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ex02
+//
+//  Created by Saeed on 10/25/20.
+//  Copyright © 2020 Saeed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
